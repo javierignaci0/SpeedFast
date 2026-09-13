db6a26e3f5eb73deeb2ae1a9944c140616df46b8
javierignaci0 <javierignacioruiza@gmail.com>
2026-09-12 22:15:45 -0300

    feat(ZonaDeCarga): Se agrega historialPedidos para funciones de historial y resumen.

--- src/ZonaDeCarga.java
+++ src/ZonaDeCarga.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class ZonaDeCarga {
@@ -16,5 +18,9 @@ public class ZonaDeCarga {
     public synchronized int cantidadPedidos(){
         return listaPedidos.size();
     }
+
+    public List<Pedido> historialPedidos = new ArrayList<>();
+
+
 
 }
